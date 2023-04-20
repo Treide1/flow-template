d0737f892d5bb3de66ca10cddf6ec07af7a6c4c5
lukas <lukas.henke.berlin@gmx.de>
2023-04-20 08:53:06 +0200

    added FxRepo comments

--- src/main/kotlin/flow/fx/FxRepo.kt
+++ src/main/kotlin/flow/fx/FxRepo.kt
@@ -4,18 +4,32 @@ import org.openrndr.draw.ColorBuffer
 import org.openrndr.draw.Filter
 import org.openrndr.extra.fx.blend.Overlay
 
+/**
+ * FxRepo is a repository for all the effects that are applied to the visual content.
+ * It allows to predefine the [chain] during config, and apply it with [applyChain].
+ */
 class FxRepo(var chain: FxRepo.() -> Unit) {
 
     private val overlay by lazy { Overlay() }
 
+    /**
+     * Applies the [overlay] filter to the [source] and [target] buffer combined,
+     * and writes the result to the [target] buffer.
+     */
     fun overlay(source: ColorBuffer, target: ColorBuffer) {
         overlay.apply(source, target)
     }
 
+    /**
+     * Applies the fx chain from [chain].
+     */
     fun applyChain() {
         chain()
     }
 
+    /**
+     * Shortcut for [Filter.apply] with (buffer, buffer).
+     */
     fun Filter.apply(buffer: ColorBuffer) {
         this.apply(buffer, buffer)
     }
