e76bc8520acf80c8bc4db55ecc9fa8c78d3ef3c8
lukas <lukas.henke.berlin@gmx.de>
2023-04-22 00:47:50 +0200

    added docs for VisualGroup

--- src/main/kotlin/flow/content/VisualGroup.kt
+++ src/main/kotlin/flow/content/VisualGroup.kt
@@ -3,6 +3,10 @@ package flow.content
 import org.openrndr.Program
 import org.openrndr.draw.Drawer
 
+/**
+ * Visual group that can be drawn.
+ * Inherit as object and implement [draw].
+ */
 abstract class VisualGroup {
     abstract fun draw(drawer: Drawer, program: Program)
 }
