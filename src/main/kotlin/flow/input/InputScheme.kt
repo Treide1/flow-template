16b1abb21a462766ac6c0aa674e233624b315972
lukas <lukas.henke.berlin@gmx.de>
2023-04-23 12:13:22 +0200

    reworked InputScheme

--- src/main/kotlin/flow/input/InputScheme.kt
+++ src/main/kotlin/flow/input/InputScheme.kt
@@ -1,54 +1,113 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 package flow.input
 
 import org.openrndr.KeyEvent
 import org.openrndr.KeyEvents
 import org.openrndr.Program
 
-// TODO: Generalize to not just keyboard.keyDown (keyUp, ... / mouse, midi, ...)
-class InputScheme(program: Program) {
+// TODO: Generalize to not just keyboard (mouse, midi, ...)
+class InputScheme(keyEvents: KeyEvents) {
 
     // Keybinds
     val keyBinder = KeyBinder()
 
+    val keyTracker = KeyTracker()
+
     init {
-        program.keyboard.keyDown.listen {
-            keyBinder.handleKeyEvent(it)
+        keyEvents.keyDown.listen { event ->
+            keyBinder.handleKeyEvent(event)
+            keyTracker.handleKeyDown(event)
+            event.cancelPropagation()
+        }
+        keyEvents.keyUp.listen { event ->
+            keyTracker.handleKeyUp(event)
+            event.cancelPropagation()
         }
     }
 
     class KeyBinder {
         val bindList = mutableListOf<KeyBinding>()
 
-        fun unbind(keyCode: Int) {
-            bindList.removeIf { it.keyCode == keyCode }
-        }
-
         fun handleKeyEvent(event: KeyEvent) {
             val options = listOf(event.key, event.name.hashCode())
             bindList.filter { it.keyCode in options }.forEach {
                 it.action()
             }
-            event.cancelPropagation()
         }
-
-        fun unbind(keyName: String) = unbind(keyName.hashCode())
 
         fun Int.bind(action: () -> Unit) = bindList.add(KeyBinding(this, action))
         fun String.bind(action: () -> Unit) = this.hashCode().bind(action)
+
+        fun unbind(keyCode: Int) = bindList.removeIf { it.keyCode == keyCode }
+
+        fun unbind(keyName: String) = unbind(keyName.hashCode())
     }
 
     fun keyboardKeyDown(bindConfig: KeyBinder.() -> Unit) {
         keyBinder.bindConfig()
     }
 
-    // Tracking
-    // TODO: allow for isActive access
-    fun KeyEvents.track(toggle: TrackTypes, vararg keyNames: String) {
+    class KeyTracker {
 
+        val pianoKeys = mutableMapOf<Int, Boolean>()
+        val toggleKeys = mutableMapOf<Int, Boolean>()
+
+        fun handleKeyDown(event: KeyEvent) {
+            val options = listOf(event.key, event.name.hashCode())
+            options.forEach { i ->
+                if (pianoKeys.containsKey(i)) {
+                    pianoKeys[i] = true
+                }
+                if (toggleKeys.containsKey(i)) {
+                    toggleKeys[i] = !toggleKeys[i]!!
+                }
+            }
+        }
+
+        fun handleKeyUp(event: KeyEvent) {
+            val options = listOf(event.key, event.name.hashCode())
+            options.forEach { i ->
+                if (pianoKeys.containsKey(i)) {
+                    pianoKeys[i] = false
+                }
+            }
+        }
+
+        fun isActive(keyCode: Int): Boolean {
+            return (pianoKeys[keyCode] ?: false) || (toggleKeys[keyCode] ?: false)
+        }
     }
 
-    fun KeyEvents.track(toggle: TrackTypes, keyNames: List<String>) {
+    // Tracking
+    // TODO: allow for isActive access
 
+    fun track(trackType: TrackTypes, keyNames: List<String>) {
+        when (trackType) {
+            TrackTypes.PIANO -> {
+                keyNames.forEach { keyName ->
+                    keyTracker.pianoKeys[keyName.hashCode()] = false
+                }
+            }
+            TrackTypes.TOGGLE -> {
+                keyNames.forEach { keyName ->
+                    keyTracker.toggleKeys[keyName.hashCode()] = false
+                }
+            }
+
+        }
+    }
+
+    fun track(trackType: TrackTypes, vararg keyNames: String) {
+        track(trackType, keyNames.toList())
+    }
+
+    fun isKeyActive(keyCode: Int): Boolean {
+        return keyTracker.isActive(keyCode)
+    }
+
+    fun isKeyActive(keyName: String): Boolean {
+        return isKeyActive(keyName.hashCode())
     }
 
     /**
@@ -57,22 +116,13 @@ class InputScheme(program: Program) {
      */
     enum class TrackTypes {
         /**
+         * Key is active as long as its pressed.
+         */
+        PIANO,
+        /**
          * Pressing toggles active on/off.
          */
         TOGGLE,
-        /**
-         * Key is active as long as its pressed.
-         */
-        PIANO
-    }
-
-    // TODO: get the active state of the key
-    fun isKeyActive(keyCode: Int) {
-
-    }
-
-    fun isKeyActive(keyName: String) {
-
     }
 
 }
@@ -88,6 +138,6 @@ data class KeyBinding(val keyCode: Int, val action: () -> Unit)
  * Setup of an [InputScheme] for a [Program].
  * Configure it with the [config] lambda.
  */
-fun Program.inputScheme(config: InputScheme.() -> Unit): InputScheme {
-    return InputScheme(this).also { config(it) }
+fun inputScheme(keyEvents: KeyEvents, config: InputScheme.() -> Unit): InputScheme {
+    return InputScheme(keyEvents).also { config(it) }
 }
