fe4f1238014b6d24005d2d8e402c465cf015f5aa
lukas <lukas.henke.berlin@gmx.de>
2023-04-23 12:16:38 +0200

    updated TemplateProgram.kt

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,5 +1,5 @@
-import flow.audio.Audio
 import flow.bpm.BeatClock
+import flow.bpm.envelope.Envelope
 import flow.bpm.toIntervalCount
 import flow.colorRepo.ColorRepo
 import flow.content.VisualGroup
@@ -7,17 +7,23 @@ import flow.fx.FxRepo
 import flow.input.InputScheme.TrackTypes.PIANO
 import flow.input.InputScheme.TrackTypes.TOGGLE
 import flow.input.inputScheme
-import org.openrndr.*
+import org.openrndr.Fullscreen
+import org.openrndr.KEY_ESCAPE
+import org.openrndr.KEY_SPACEBAR
+import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.*
 import org.openrndr.extra.fx.blur.ApproximateGaussianBlur
 import org.openrndr.extra.fx.blur.FrameBlur
 import org.openrndr.extra.fx.blur.GaussianBloom
 import org.openrndr.math.Vector2
-import kotlin.math.pow
+import org.openrndr.math.map
+import util.Capacitor
+import util.lerp
+import kotlin.math.*
 
 /**
- * Template for "Flow"
+ * Template for "Flow" programs.
  */
 fun main() = application {
     configure {
@@ -27,18 +33,19 @@ fun main() = application {
     program {
 
         // Input Scheme (1st half)
-        val inputScheme = inputScheme {
+        val inputScheme = inputScheme(keyboard) {
             // Tracked keys
-            keyboard.track(TOGGLE, "qe".split(""))
-            keyboard.track(PIANO, "wasd".split(""))
+            track(TOGGLE, "q", "e")
+            track(PIANO, "wasd".split(""))
         }
 
         // Init beatClock
-        val beatClock = extend(BeatClock(bpm = 120.0))
+        val beatClock = extend(BeatClock(bpm = 125.0))
 
         // Init beat-based values
         val kick by beatClock.bindEnvelopeBySegments(length = 1.0) {
-            segmentJoin(0.01, 1.0)
+            segmentJoin(0.15, 0.8)
+            segmentJoin(0.2, 1.0)
             segmentJoin(1.0, 0.0) via { x: Double -> x.pow(3.0) }
         }
         val flash by beatClock.bindEnvelope(1/2.0) { phase ->
@@ -48,14 +55,7 @@ fun main() = application {
         }
 
         // Init audio input
-        val audio = extend(Audio())
-
-        // TODO: add volume samples for ranges
-        audio.ranges = listOf(
-            20.0 .. 200.0,
-            500.0 .. 2500.0,
-            Audio.TREBLE,
-        )
+        // TODO: AUDIO
 
         // Init colors
         val colorRepo = ColorRepo {
@@ -86,8 +86,12 @@ fun main() = application {
         }
 
         // Init visual groups
-        val circleGroup = object: VisualGroup() {
-            override fun draw(drawer: Drawer, program: Program) {
+        val circleGroup = object: VisualGroup(program) {
+            val sizeRange = 25.0..35.0
+
+            override fun Drawer.draw() {
+
+                // Draw a circle at each corner of the screen
                 listOf(
                     Vector2(0.2, 0.2),
                     Vector2(0.2, 0.8),
@@ -95,23 +99,80 @@ fun main() = application {
                     Vector2(0.8, 0.2)
                 ).forEachIndexed { i, (x, y) ->
                     val colorIndex = i % 2 + 1
-                    drawer.fill = colorRepo.palette[colorIndex].opacify(flash)
-                    drawer.stroke = null
-                    drawer.circle(x * width, y * height, kick * 50.0 + 100.0)
+                    var color = colorRepo.palette[colorIndex]
+                    if (inputScheme.isKeyActive("e")) color = color.opacify(flash)
+                    fill = color
+                    stroke = null
+                    val r = kick.map(0.0, 1.0, sizeRange.start, sizeRange.endInclusive)
+                    circle(x * width, y * height, r)
+                }
+
+                // Draw a circle in each diamond direction on screen
+                val flashGate = if (inputScheme.isKeyActive("e")) flash else 0.0
+                val relR = max(kick, flashGate).map(0.0, 1.0, 0.15, 0.35)
+                "wasd".split("").forEachIndexed { i, key ->
+                    val isActive = inputScheme.isKeyActive(key)
+                    if (!isActive) return@forEachIndexed
+                    val pos = drawer.bounds.center + Vector2(0.0, -height*relR).rotate(90 - i * 90.0)
+                    fill = colorRepo.palette[0]
+                    circle(pos, sizeRange.endInclusive)
                 }
             }
         }
 
-        val diamondGroup = object: VisualGroup() {
-            override fun draw(drawer: Drawer, program: Program) {
-                drawer.isolated {
-                    fill = colorRepo.palette[0].opacify(kick*0.5 + 0.5)
+        val diamondGroup = object: VisualGroup(program) {
+            val mainSize = 75.0
+
+            val ringCount = 12
+            val ringRadius = 100.0
+            val ringSize = 10.0
+            var ringRot = 0.0
+
+            override fun Drawer.draw() {
+                // Main diamond
+                val mainColor = colorRepo.palette[0].opacify(kick*0.5 + 0.5)
+                fill = mainColor
+                stroke = null
+                drawDiamond(width/2.0, height/2.0, mainSize)
+
+                // If "q" is pressed, draw a ring of diamonds around the main diamond
+                val ringOpacity = Capacitor(0.0, 0.8).apply {
+                    onGateOpen = Envelope(1.0) { t ->
+                        if (t < 0.4) (t / 0.4).pow(1/2.0) // Reaches 1.0
+                        else 1.0.lerp(holdValue, (t - 0.4) / 0.1)
+                    }
+                    onGateClosed = Envelope (0.5) { t ->
+                        holdValue.lerp(offValue, t / 0.2)
+                    }
+                }
+
+                ringOpacity.update(program.seconds, inputScheme.isKeyActive("q"))
+
+                // Draw a ring of diamonds around the main diamond
+                ringRot += kick * 0.05
+                val size = ringSize * if (inputScheme.isKeyActive("e")) flash else 1.0
+                //drawDiamondRing(bounds.center, ringCount, ringRadius, size, ringRot)
+                val center = bounds.center
+                val angleStep = PI * 2 / ringCount
+                for (i in 0 until ringCount) {
+                    val angle = i * angleStep + ringRot
+                    val x = center.x + ringRadius * cos(angle)
+                    val y = center.y + ringRadius * sin(angle)
+                    fill = colorRepo.palette[i%2 + 1].opacify(ringOpacity.value)
                     stroke = null
-                    translate(width/2.0, height/2.0)
+                    drawDiamond(x, y, size)
+                }
+
+            }
+
+            fun Drawer.drawDiamond(x: Double, y: Double, size: Double) {
+                this.isolated {
+                    translate(x, y)
                     rotate(45.0)
-                    rectangle(-100.0, -100.0, 200.0, 200.0)
+                    rectangle(-size/2.0, -size/2.0, size, size)
                 }
             }
+
         }
 
         // Draw loop
@@ -119,9 +180,11 @@ fun main() = application {
             drawer.isolatedWithTarget(rt) {
                 clear(ColorRGBa.TRANSPARENT)
 
-                circleGroup.draw(drawer, program)
-                diamondGroup.draw(drawer, program)
+                // Draw visual groups
+                circleGroup.draw()
+                diamondGroup.draw()
             }
+            // Apply Fx and draw to screen
             fxRepo.applyChain()
             drawer.image(joinBuffer)
         }
@@ -132,7 +195,8 @@ fun main() = application {
             keyboardKeyDown {
                 KEY_ESCAPE.bind { application.exit() }
                 KEY_SPACEBAR.bind { beatClock.resetTime(program.seconds) }
-                "k".bind { beatClock.animateTo(bpm = 60.0, program.seconds, 1.0) }
+                "k".bind { beatClock.animateTo(bpm = beatClock.bpm / 2.0, program.seconds, 0.1) }
+                "l".bind { beatClock.animateTo(bpm = beatClock.bpm * 2.0, program.seconds, 0.1) }
             }
         }
 
