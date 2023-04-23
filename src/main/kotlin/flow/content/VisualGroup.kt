cb6791e9068871312d57b63bb06e9e223b6c3135
lukas <lukas.henke.berlin@gmx.de>
2023-04-23 12:13:40 +0200

    refactored VisualGroup

--- src/main/kotlin/flow/content/VisualGroup.kt
+++ src/main/kotlin/flow/content/VisualGroup.kt
@@ -7,6 +7,15 @@ import org.openrndr.draw.Drawer
  * Visual group that can be drawn.
  * Inherit as object and implement [draw].
  */
-abstract class VisualGroup {
-    abstract fun draw(drawer: Drawer, program: Program)
+abstract class VisualGroup(program: Program) {
+
+    val drawer = program.drawer
+    abstract fun Drawer.draw()
+
+    fun draw() {
+        drawer.pushStyle()
+        drawer.draw()
+        drawer.popStyle()
+    }
+
 }
