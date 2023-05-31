7fdc5d4fe280d1d50cd484f78f7adf60feddc391
lukas <lukas.henke.berlin@gmx.de>
2023-05-31 17:29:07 +0200

    added docs for FlowProgram

--- src/main/kotlin/flow/FlowProgram.kt
+++ src/main/kotlin/flow/FlowProgram.kt
@@ -26,105 +26,101 @@ data class FlowProgramConfig(
 )
 
 /**
+ * The [FlowProgram] is the main class of the Flow framework.
  *
+ * It provides several properties to access API instances like [beatClock], [renderPipeline] and many more.
+ *
+ * @author Lukas Henke, 31.05.23
  */
 open class FlowProgram private constructor(
     val config: FlowProgramConfig = FlowProgramConfig(),
+    private val init: suspend FlowProgram.() -> Unit,
 ): ProgramImplementation(suspend = false) {
 
     /**
-     *
+     * The [FlowProgram] instance. More readable than `this` in program block.
      */
     val flowProgram = this
 
     /**
-     *
+     * [BeatClock] instance.
      */
-    val beatClock = extend(BeatClock(bpm= config.initialBpm))
+    val beatClock = extend(BeatClock(bpm = config.initialBpm))
 
     /**
-     *
+     * Standard OPENRNDR [GUI] instance. Only used if configured in [config].
      */
     val gui by lazy { GUI() }
 
     /**
-     *
+     * [RenderPipeline] instance. Used to render the final image.
      */
     val renderPipeline by lazy { RenderPipeline(width, height, flowProgram) }
 
-    // TODO: provide access to attachments like this:
-    /*{
-        attachDrawer("main")
-        attachDrawer("blend")
-        attachDrawer("tmp")
-        attachDrawer("image", out = true)
-        attachStencil("stencil")
-    }*/
-
     /**
-     *
+     * [Audio] instance. Can be configured in [config].
      */
     val audio by lazy { config.audio ?: Audio() }
 
     /**
-     *
+     * [InputScheme] instance. Uses [keyboard] by default.
      */
     val inputScheme = InputScheme(keyboard)
 
     /**
-     *
+     * [UiDisplay] instance.
      */
     val uiDisplay = UiDisplay(inputScheme).apply {
+        // Default display values.
         trackValue("BPM") { "${beatClock.bpm}" }
         trackValue("FPS") { "${beatClock.fps}" }
     }
 
-    /**
-     *
-     */
-    internal fun beforeInit() {
+    override suspend fun setup() {
+        super.setup()
+
+        // BEFORE INIT
         extend(AutoUpdate)
 
-        uiDisplay.alphaCap = LinearCapacitor(0.1, 0.5, offValue = 1.0, holdValue = 0.0).keyAutoUpdate(this, "f1")
+        // Define UiDisplay fade in / fade out
+        uiDisplay.alphaCap = LinearCapacitor(0.1, 0.5, offValue = 1.0, holdValue = 0.0).keyAutoUpdate(flowProgram, "f1")
 
-        // Define controls for Input Scheme
+        // Define controls for InputScheme
         inputScheme.apply {
             // Tracked keys
             track(InputScheme.TrackTypes.TOGGLE, "f1", "Toggle this controls display")
 
-            // Hard-coded input bindings
+            // Default input bindings
             keyDown {
-                KEY_ESCAPE.bind("Exit Application") { audio.stop(); application.exit() }
+                KEY_ESCAPE.bind("Exit Application") { flowProgram.exit() }
                 KEY_SPACEBAR.bind("Reset Beat Clock") { beatClock.resetTime() }
             }
         }
+
+        // INIT
+        init()
+
+        // AFTER INIT
+        // extend gui after values have been added in init
+        if (config.isWithGui) extend(gui)
     }
 
-    /**
-     *
-     */
-    fun afterInit() {
-        if (config.isWithGui) extend(gui)
+    fun exit() {
+        audio.stop()
+        application.exit()
     }
 
     companion object {
 
         /**
-         *
+         * Creates and runs a synchronous [FlowProgram].
+         * @param config Optional [FlowProgramConfig].
          */
         fun ApplicationBuilder.flowProgram(
             config: FlowProgramConfig = FlowProgramConfig(),
             init: suspend FlowProgram.() -> Unit
         ): FlowProgram {
-
-            program = object : FlowProgram(config) {
-                override suspend fun setup() {
-                    super.setup()
-                    beforeInit()
-                    init()
-                    afterInit()
-                }
-            }
+            program = FlowProgram(config, init)
             return program as FlowProgram
         }
     }
