8787e83aa502557bc8869fd49bbabfee899e8982
lukas <lukas.henke.berlin@gmx.de>
2023-05-31 17:25:23 +0200

    updated docs for SquircleBlend

--- src/main/kotlin/flow/fx/SquircleBlend.kt
+++ src/main/kotlin/flow/fx/SquircleBlend.kt
@@ -7,18 +7,24 @@ import org.openrndr.extra.parameters.IntParameter
 import org.openrndr.resourceText
 
 /**
- * Squircle Blend
+ * SquircleBlend Transition Filter.
  *
- * @param blend How much to blend the squircle effect from source0 to source1
- * @param mode The squircle shape mode
+ * Blends two images using a [squircle](https://en.wikipedia.org/wiki/Squircle)-like lense.
  */
 @Description("Squircle Blend")
 class SquircleBlend: Filter2to1(
     filterShaderFromCode(resourceText("/squircleBlend.glsl"), "squircleBlend")
 ) {
+
+    /**
+     * How much to blend the squircle effect from source0 to source1
+     */
     @DoubleParameter("blend", 0.0, 1.0)
     var blend: Double by parameters
 
+    /**
+     * The squircle shape mode for different amount/placement/curvature of squircle lenses.
+     */
     @IntParameter("mode", 0, 17)
     var mode: Int by parameters
 
