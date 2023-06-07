09789c46251c964cb0a2eec15ac52010636a596f
lukas <lukas.henke.berlin@gmx.de>
2023-06-07 13:43:20 +0200

    updated MedianDenoisingFilter to be Filter1to1

--- src/main/kotlin/flow/fx/MedianDenoisingFilter.kt
+++ src/main/kotlin/flow/fx/MedianDenoisingFilter.kt
@@ -1,9 +1,9 @@
 package flow.fx
 
-import org.openrndr.draw.Filter
+import org.openrndr.draw.Filter1to1
 import org.openrndr.draw.filterShaderFromCode
 import org.openrndr.resourceText
 
-class MedianDenoisingFilter : Filter(
+class MedianDenoisingFilter : Filter1to1(
     filterShaderFromCode(resourceText("/medianDenoising.glsl"), "medianDenoising")
 )
