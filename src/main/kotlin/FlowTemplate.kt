c65d30827b499ded8222e1890df2dd40f1556667
lukas <lukas.henke.berlin@gmx.de>
2023-06-22 09:25:48 +0200

    fixed FlowTemplate and moved old to demo 3

--- src/main/kotlin/FlowTemplate.kt
+++ src/main/kotlin/FlowTemplate.kt
@@ -1,23 +1,17 @@
 import flow.FlowProgram.Companion.flowProgram
 import flow.color.ColorRepo
-import flow.fx.fluidSim.FluidSimulation
-import flow.fx.retroSun.RetroSun
-import flow.shadertoy.ProjectRenderer
-import flow.util.CyclicFlag
+import flow.ui.UiDisplay
+import org.openrndr.Fullscreen
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.tint
 
 /**
  * Template for "Flow" programs.
  */
 fun main() = application {
     configure {
-        //fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
-        width = 1280
-        height = 720
+        fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
         display = displays.last()
-        title = "OPENRNDR ~ Shadertoy Parser"
     }
     flowProgram {
         // Init colors
@@ -34,78 +28,40 @@ fun main() = application {
         // Define visual groups
         // ...
 
-        ////////////////////////////
-
-        val fluidSim = FluidSimulation(this)
-        val retroSun = RetroSun(this)
-
-        val shader = CyclicFlag(retroSun, fluidSim)
-
-        var active: ProjectRenderer = shader.value
-        var from: ProjectRenderer? = null
-        var transition: Double? = null
-
-        fun queueTransition() {
-            from = active
-            shader.next()
-            active = shader. value
-            transition = 1.0
-        }
-
         // Define controls for Input Scheme
         inputScheme.apply {
             // Tracked keys
+            // ...
 
             // Hard-coded input bindings
             keyDown {
-                "s".bind("shader") { queueTransition() }
+                // ...
             }
         }
 
         // Init UI display
-        uiDisplay.apply {
-            trackValue("frameCount") { "$frameCount" }
-
-            // Shadertoy combiner stats
-            trackValue("iResolution") { "${fluidSim.iResolution}" }
-            trackValue("iFrame") { "${fluidSim.iFrame}" }
-            trackValue("iTime") { "${fluidSim.iTime}" }
-            trackValue("iTimeDelta") { "${fluidSim.iTimeDelta}" }
-            trackValue("iMouse") {
-                fluidSim.iMouse.run {
-                    "x: ${x.toInt()}, " +
-                    "y: ${y.toInt()}, " +
-                    "z: ${z.toInt()}, " +
-                    "w: ${w.toInt()}"
-                }
-            }
+        val uiDisplay = UiDisplay(inputScheme).apply {
+            // ...
         }
 
         // Draw loop
         extend {
+            renderPipeline.render {
+                drawBuffer.clear()
 
-            if (transition == null) {
-                drawer.image(active.render())
-            }
-            else {
-                transition = transition!! * 0.99
+                // Draw visual groups
+                // ...
 
-                val t = transition!!
+                // Use fx
+                // ...
 
-                drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.opacify(t))
-                drawer.image(from!!.render())
-
-                drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.opacify(1-t))
-                drawer.image(active.render())
-
-                if (transition!! <= 0.01) {
-                    transition = null
-                    from = null
-                }
+                // Eventually draw to image buffer
+                drawBuffer.copyTo(imageBuffer)
             }
 
             // Draw controls
             uiDisplay.displayOnDrawer(drawer)
         }
+
     }
 }
