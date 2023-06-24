4fe07b740f67d62a6a00bbebec0d32f63563c543
lukas <lukas.henke.berlin@gmx.de>
2023-06-24 12:30:08 +0200

    updated renderer examples

--- src/main/kotlin/flow/fx/fluidSim/FluidSimulation.kt
+++ src/main/kotlin/flow/fx/fluidSim/FluidSimulation.kt
@@ -1,21 +1,33 @@
 package flow.fx.fluidSim
 
-import flow.shadertoy.GlslFileBuilder
-import flow.shadertoy.Importer
+import flow.FlowProgram
+import flow.autoupdate.AutoUpdate.autoUpdate
+import flow.shadertoy.ProjectImporter
 import flow.shadertoy.ProjectRenderer
+import flow.shadertoy.ShadertoyProject
 import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.Channel.CHANNEL_0
 import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.Channel.CHANNEL_1
 import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.ChannelInput.*
-import org.openrndr.Program
-import org.openrndr.application
+import org.openrndr.animatable.easing.CubicIn
 
 /**
  *
  */
-class FluidSimulation(program: Program): ProjectRenderer(program, Importer.fromJson("/generated/fluidSim")) {
+class FluidSimulation(program: FlowProgram): ProjectRenderer(program) {
+
+    override fun importProject(): ShadertoyProject {
+        val original =  ProjectImporter.import("fluidSim", "src/main/resources/fluidSim")
+        original.bufferA!!.setInput(CHANNEL_0, BUFFER_C_IN)
+        original.bufferB!!.setInput(CHANNEL_0, BUFFER_A_IN)
+        original.bufferC!!.setInput(CHANNEL_0, BUFFER_B_IN)
+        original.bufferD!!.setInput(CHANNEL_0, BUFFER_A_IN).setInput(CHANNEL_1, BUFFER_D_IN)
+        original.image    .setInput(CHANNEL_0, BUFFER_D_IN)
+
+        return ProjectImporter.generateAndImport(original, ".")
+    }
 
     // parameters and init
-    var regionSize: Double by params
+    var regionSize: Double by parameters
 
     val attack: Double by program.beatClock.bindEnvelopeBySegments(4.0) {
         segmentJoin(0.1, 1.0)
--- src/main/kotlin/flow/fx/retroSun/RetroSun.kt
+++ src/main/kotlin/flow/fx/retroSun/RetroSun.kt
@@ -1,30 +1,20 @@
 package flow.fx.retroSun
 
-import flow.shadertoy.GlslFileBuilder
-import flow.shadertoy.Importer
+import flow.shadertoy.ProjectImporter
 import flow.shadertoy.ProjectRenderer
+import flow.shadertoy.ShadertoyProject
 import org.openrndr.Program
-import org.openrndr.application
 
 /**
  *
  */
-class RetroSun(program: Program): ProjectRenderer(program, Importer.fromJson("/generated/retroSun")) {
+class RetroSun(program: Program): ProjectRenderer(program) {
+
+    override fun importProject(): ShadertoyProject {
+        val original = ProjectImporter.import("retroSun", "src/main/resources/retroSun")
+        return ProjectImporter.generateAndImport(original, ".")
+    }
 
     // parameters and init
 
-}
-
-/**
- * Generate files
- */
-fun main(args: Array<String>) {
-    application {
-        program {
-            val project =  Importer.fromResourceDirectory("retroSun", "/retroSun")
-            val builder = GlslFileBuilder(project)
-            builder.generate() // Only required to be executed once
-            application.exit()
-        }
-    }
 }
