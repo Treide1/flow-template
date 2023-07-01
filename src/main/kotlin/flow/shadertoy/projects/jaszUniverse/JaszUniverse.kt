68217196e1b6221fb500ecc20a657303c82168b1
lukas <lukas.henke.berlin@gmx.de>
2023-07-01 11:22:55 +0200

    prepared JaszUniverse for mic usage

--- src/main/kotlin/flow/shadertoy/projects/jaszUniverse/JaszUniverse.kt
+++ src/main/kotlin/flow/shadertoy/projects/jaszUniverse/JaszUniverse.kt
@@ -1,22 +1,24 @@
 package flow.shadertoy.projects.jaszUniverse
 
+import flow.FlowProgram
 import flow.shadertoy.ProjectImporter
 import flow.shadertoy.ProjectRenderer
 import flow.shadertoy.ShadertoyProject
 import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.Channel.CHANNEL_0
-import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.ChannelInput.BUFFER_A_IN
 import flow.shadertoy.ShadertoyProject.ShadertoyTab.ChannelSettings.ChannelInput.MICROPHONE
-import org.openrndr.Program
 
 /**
  * A renderer for the shadertoy project ["Retro Sun Modified" by JennySchub](https://www.shadertoy.com/view/WdGSzz).
  */
-class JaszUniverse(program: Program): ProjectRenderer(program) {
+class JaszUniverse(val flowProgram: FlowProgram): ProjectRenderer(flowProgram) {
 
     override fun ProjectImporter.importProject(): ShadertoyProject {
 
         return buildAndImport("$projectsPath/jaszUniverse", "jaszUniverse") {
             image.setInput(CHANNEL_0, MICROPHONE)
+
+            // TODO: Allow registering microphone
+            // Wishful code: registerMicrophone(flowProgram.audio.magnitudes.format(512, 2))
         }
     }
 
